53c4c18877f9030a22ff12a5543c169b1fea3b66
Chandan <kmchandu@outlook.com>
2019-11-23 02:26:38 +0530

    mobile-first and image resolution added

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,12 @@ var gulp = require("gulp"),
 watch =require("gulp-watch"),
 postcss = require("gulp-postcss"),
 autoprefixer = require("autoprefixer"),
-cssvars=require("postcss-simple-vars");
-nested=require("postcss-nested")
+cssvars=require("postcss-simple-vars"),
+nested=require("postcss-nested"),
+cssImport =require("postcss-import"),
+browseSync = require("browser-sync").create(),
+mixins = require("postcss-mixins");
+
 
 gulp.task('default',function(){
 console.log("We created the gulp default task");
@@ -15,19 +19,30 @@ gulp.task('html',function(){
 
 gulp.task('style',function(){
     return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssvars,nested,autoprefixer]))
+    .pipe(postcss([cssImport,mixins,cssvars,nested,autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'))
 });
 
-gulp.task('watch',function(){
-    console.log("started");
+gulp.task('watch',function()
+{
+    browseSync.init({
+        server:{
+            baseDir : "app"    
+        }
+    });
     watch('./app/index.html',function(){
+        browseSync.reload();
         gulp.start('html');
     });
 
     watch('./app/assets/styles/**/*.css',function(){
-        gulp.start('style');
+        gulp.start('cssInject');
     });
 });
+
+gulp.task('cssInject',['style'],function(){
+    return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browseSync.stream());
+})
 
 
