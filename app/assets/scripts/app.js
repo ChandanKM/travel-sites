932cb9e2e5ab96aca6e684ec53ee9e48bac13974
Chandan <kmchandu@outlook.com>
2019-12-11 23:25:09 +0530

    Reveal on change is complited

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,5 +1,7 @@
+import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 
 var mobileMenu = new MobileMenu();
-var revealOnScroll = new RevealOnScroll();
+new RevealOnScroll($(".feature-item"),"85%");
+new RevealOnScroll($(".testimonial"),"60%");
--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -2,8 +2,9 @@ import $ from 'jquery';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 
 class RevealOnScroll {
-  constructor() {
-    this.itemsToReveal = $(".feature-item");
+  constructor(els,offset) {
+    this.itemsToReveal = els;
+    this.offsetPercentage = offset;
     this.hideInitially();
     this.createWaypoints();
   }
@@ -13,6 +14,7 @@ class RevealOnScroll {
   }
 
   createWaypoints() {
+    var that = this;
     this.itemsToReveal.each(function() {
       var currentItem = this;
       new Waypoint({
@@ -20,7 +22,7 @@ class RevealOnScroll {
         handler: function() {
           $(currentItem).addClass("reveal-item--is-visible");
         },
-        offset: "85%"
+        offset: that.offsetPercentage
       });
     });
   }
